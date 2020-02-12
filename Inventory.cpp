23c70bd3e78fdccb7ea0b2dc713ec4d080853fa3
Scott Roes <scottroes@wlan-t-238-117-dhcp.CSUChico.EDU>
2020-02-12 11:33:54 -0800

    Updated Inventory

--- Inventory.cpp
+++ Inventory.cpp
@@ -14,7 +14,9 @@ Inventory::Inventory(string name, float price, int count)
 
 void Inventory::sell()
 {
-  m_in_stock--;
+ std::cout << "Sorry, that item is out of stock";
+
+//  m_in_stock;
 }
 
 ostream& operator<<(ostream& stream, const Inventory& item)
